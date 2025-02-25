b947bc0c62df33bc1575a4b9460beb3af3f74348
fahadbgnr <aminulislamfahad1@gmail.com>
2025-02-25 18:26:39 +0600

    my add money sction done

---
+++ script/toggle.js
@@ -0,0 +1,17 @@
+
+document.getElementById('chasout').style.display = 'none';
+
+
+
+document.getElementById('add-money-box')
+.addEventListener('click', function(){
+    document.getElementById('chasout').style.display = 'none';
+    document.getElementById('Add-money').style.display = 'block';
+})
+
+
+document.getElementById('cashout-box')
+.addEventListener('click', function(){
+    document.getElementById('chasout').style.display = 'block';
+    document.getElementById('Add-money').style.display = 'none';
+})
